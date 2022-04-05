90cc6c069d731374b2e0627eb7bc3c1c8b8d99bb
thisisaaronland <aaron@localhost>
2022-04-05 11:43:22 -0700

    snapshot: prune more

--- static/javascript/js_-CPIpBjXzxABQv31RXcqizSgaLzthEF_xbzTZvR1Nak.pruned.js
+++ static/javascript/js_-CPIpBjXzxABQv31RXcqizSgaLzthEF_xbzTZvR1Nak.pruned.js
@@ -1,40 +1,12 @@
 /* necessary for the hover menus */
-
 /*
  * Supposition v0.2 - an optional enhancer for Superfish jQuery menu widget.
- *
- * Copyright (c) 2008 Joel Birch - based mostly on work by Jesse Klaasse and credit goes largely to him.
- * Special thanks to Karl Swedberg for valuable input.
- *
- * Dual licensed under the MIT and GPL licenses:
- *  http://www.opensource.org/licenses/mit-license.php
- *  http://www.gnu.org/licenses/gpl.html
- */
-/*
  * This is not the original jQuery Supposition plugin.
- * Please refer to the README for more information.
- */
-
-
-/* necessary for the hover menus */
-
-/**
- * @file
- * The Superfish Drupal Behavior to apply the Superfish jQuery plugin to lists.
- */
+*/
 
 (function ($, Drupal, drupalSettings) {
 
     'use strict';
-
-    /**
-     * jQuery Superfish plugin.
-     *
-     * @type {Drupal~behavior}
-     *
-     * @prop {Drupal~behaviorAttach} attach
-     *   Attaches the behavior to an applicable <ul> element.
-     */
 
     Drupal.behaviors.superfish = {
         attach: function (context, drupalSettings) {
@@ -69,3 +41,4 @@
         }
     };
 })(jQuery, Drupal, drupalSettings);
+
